e31c87f115ac7ec3c8c6671047371616e57c37b1
Fran <franencabo@gmail.com>
2021-12-08 09:24:27 +0100

    añadimos ask items al pulsar boton

---
+++ app.js
@@ -0,0 +1,44 @@
+// Define UI vars
+
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Call to Load all eventListeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  form.addEventListener("submit", addTask);
+}
+
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "collection-item";
+  // Create the textNode and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append the li to ul
+  taskList.appendChild(li);
+
+  // Clear the input
+  taskInput.value = "";
+
+  e.preventDefault();
+}
